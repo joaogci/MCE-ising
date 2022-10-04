4bfc25b00d2fb368a5530303ea9ac317609e1943
João Inácio <j.g.c.inacio@fys.uio.no>
2022-10-04 15:41:05 +0200

    scirpt works for MCE

--- therm_ising.py
+++ therm_ising.py
@@ -1,4 +1,3 @@
-import enum
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
@@ -6,23 +5,43 @@ import matplotlib
 plt.style.use('seaborn')
 matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['font.family'] = 'STIXGeneral'
+matplotlib.rcParams['lines.marker'] = '.'
+matplotlib.rcParams['lines.linestyle'] = '-'
+matplotlib.rcParams['lines.linewidth'] = '1'
+matplotlib.rcParams['lines.markersize'] = '5'
+matplotlib.rcParams['savefig.bbox'] = 'tight'
+
+SAVE_FOLDER = "figures/"
+SAVE_EXT = ".pdf"
+
+NN_list = {"SS": 4, "SC": 6, "HCP": 12, "Hex": 8, "FCC": 12, "BCC": 8}
+N_list = {"SS": lambda L: L**2, "SC": lambda L: L**3, "HCP": lambda L: 4*L**3, "Hex": lambda L: L**3, "FCC": lambda L: 4*L**3, "BCC": lambda L: 2*L**3}
 
 # System information
-L = 4
-N = L**2
+L = 16
 lattice = "SS"
-NN = 4 if lattice == "SS" else 6
-log_REP = 4
+Sz_vals = 2
+
+N = N_list[lattice](L)
+NN = NN_list[lattice]
+S = (Sz_vals - 1.0) / 2.0
 
 J = 1.0
 
-T = np.linspace(0.1, 5.0, 32)
-H = np.linspace(0.0, 1.0, 32)
+T_max = 5.0
+dT = 0.05
+H_max = 0.1
+dH = 0.002
+
+T = np.arange(dT, T_max + dT, dT)
+H = np.arange(0.0, H_max + dH, dH)
+# H = [0.0, 0.05, 0.1]
+# dH = 0.05
 T_vals = len(T)
 H_vals = len(H)
 
 print("Computing thermodynamic variables for: ")
-print(f" L: {L} | lattice: {lattice} | J: {J}")
+print(f" L: {L} | lattice: {lattice} | N: {N} | S: {S} | J: {J}")
 print(f" Ti: {T[0]} | Tf: {T[-1]} | nT: {len(T)}")
 print(f" Hi: {H[0]} | Hf: {H[-1]} | nH: {len(H)}")
 
@@ -31,12 +50,17 @@ def Hamiltonian(J, E, h, M):
     return J * E - h * M
 
 # Energy and magnetization values and JDOS
-E = np.arange(- J * NN * N / 2, J * NN * N / 2 + 4 * J, 4 * J)
-M = np.arange(- N, N + 1, 2)
-M_vals = len(M)
-E_vals = len(E)
+max_E = 4.0 * J * S**2 * NN * N / 2.0
+dE = 4 * J
+max_M = 2 * S * N
+dM = 2
 
-JDOS_filename = "JDOS/JDOS_L" + str(L) + "_" + lattice + "_npos2_R1E" + str(log_REP) + ".txt"
+E_sys = np.arange(- max_E, max_E + dE, dE)
+M_sys = np.arange(- max_M, max_M + dM, dM)
+M_vals = len(M_sys)
+E_vals = len(E_sys)
+
+JDOS_filename = "JDOS/JDOS_L" + str(L) + "_" + lattice + "_npos" + str(Sz_vals) + ".dat"
 print(f"Reading JDOS from file: {JDOS_filename}")
 
 g = np.loadtxt(JDOS_filename)
@@ -45,111 +69,132 @@ if g[0, -1] == 0:
 print("JDOS read")
 
 # Partition function
-Z = np.zeros((H_vals, T_vals))
-ZM = np.zeros((H_vals, T_vals, M_vals))
-
+ln_ZM = np.zeros((H_vals, T_vals, M_vals))
 for j, h in enumerate(H):
     for i, t in enumerate(T):
-        for q, m in enumerate(M):
-            ZM[j, i, q] = np.sum(g[:, q] * np.exp(- Hamiltonian(J, E, h, m) / t))
-        Z[j, i] += np.sum(ZM[j, i, :])
-
+        for q, m in enumerate(M_sys):
+            hits = np.where(g[:, q] != 0.0)[0]
+            
+            ln_ZM[j, i, q] += np.log(g[hits[0], q]) - Hamiltonian(J, E_sys[hits[0]], h, m) / t
+            ln_ZM[j, i, q] += np.log(1 + np.sum(np.exp(np.log(g[hits[1:], q]) - (Hamiltonian(J, E_sys[hits[1:]], h, m) / t) - ln_ZM[j, i, q])))
+        
 print("Computed partition function")
-
-# Ensemble averages
-# Energies
-E_mean = np.zeros((H_vals, T_vals))
-E2_mean = np.zeros((H_vals, T_vals))
-C_mean = np.zeros((H_vals, T_vals))
-
-for j, h in enumerate(H):
-    for i, t in enumerate(T):
-        for idx, e in enumerate(E):
-            E_mean[j, i] += e * np.sum(g[idx, :] * np.exp(- Hamiltonian(J, e, h, M) / t)) / Z[j, i]
-            E2_mean[j, i] += e**2 * np.sum(g[idx, :] * np.exp(- Hamiltonian(J, e, h, M) / t)) / Z[j, i]
-        C_mean[j, i] = (E2_mean[j, i] - E_mean[j, i]**2) / t**2
-
-E_mean /= N
-E2_mean /= N
-C_mean /= N
-
-# Magnetizations
-M_mean = np.zeros((H_vals, T_vals))
-M2_mean = np.zeros((H_vals, T_vals))
-M4_mean = np.zeros((H_vals, T_vals))
-Mabs_mean = np.zeros((H_vals, T_vals))
-X_mean = np.zeros((H_vals, T_vals))
-
-for j, h in enumerate(H):
-    for i, t in enumerate(T):
-        for q, m in enumerate(M):
-            M_mean[j, i] += m * ZM[j, i, q] / Z[j, i]
-            M2_mean[j, i] += m**2 * ZM[j, i, q] / Z[j, i]
-            M4_mean[j, i] += m**4 * ZM[j, i, q] / Z[j, i]
-            Mabs_mean[j, i] += np.abs(m) * ZM[j, i, q] / Z[j, i]
-        X_mean[j, i] = (M2_mean[j, i] - M_mean[j, i]**2) / t
-
-M_mean /= N
-M2_mean /= N
-M4_mean /= N
-Mabs_mean /= N
-X_mean /= N
-
-print("Computed ensemble average variables")
 
 # Minimization of G
 G_tmp = np.zeros((H_vals, T_vals, M_vals))
 G = np.zeros((H_vals, T_vals))
+M = np.zeros((H_vals, T_vals))
 for j, h in enumerate(H):
-    for q, m in enumerate(M):
-        G_tmp[j, :, q] = - T * np.log(ZM[j, :, q])
+    for q, m in enumerate(M_sys):
+        G_tmp[j, :, q] = - T * ln_ZM[j, :, q]
 
-    G[j, :] = np.min(G_tmp[j, :, :], axis=1)
+        G[j, :] = np.min(G_tmp[j, :, :], axis=1)
+        M[j, :] = np.abs(M_sys[np.argmin(G_tmp[j, :, :], axis=1)])
 
 G /= N
+M /= N
 
-M_F = - np.gradient(G, H, axis=0)
-S_F = - np.gradient(G, T, axis=1)
-C_F = - T * np.gradient(np.gradient(G, T, axis=1), T, axis=1)
-X_F = - np.gradient(np.gradient(G, H, axis=0), H, axis=0)
-U_F = np.zeros((H_vals, T_vals))
+S = - np.gradient(G, T, axis=1)
+C = T * np.gradient(S, T, axis=1)
+X = np.gradient(M, H, axis=0)
+U = np.zeros((H_vals, T_vals))
 for j, h in enumerate(H):
-    U_F[j, :] = G[j, :] + T * S_F[j, :] + H[j] * M_F[j, :]
+    U[j, :] = G[j, :] + T * S[j, :]
 
 print("Computed minimization of G and variables")
 
-plt.figure(1)
-plt.plot(T, M_F[0, :], label="from F min")
-plt.plot(T, Mabs_mean[0, :], label="from T avg")
-plt.xlabel(r"$T$")
-plt.ylabel(r"$|m|$")
-plt.legend()
+dSM = np.zeros((H_vals, T_vals))
+dSM_2 = np.zeros((H_vals, T_vals))
+dT = np.zeros((H_vals, T_vals))
+dM_dT_H = np.gradient(M, T, axis=1)
 
-plt.figure(2)
-plt.plot(T, S_F[0, :], label="from F min")
+for i, t in enumerate(T):
+    for j, h in enumerate(H):
+        H_new = np.arange(H[0], H[j], dH / 1000)
+
+        dSM[j, i] = np.trapz(np.interp(H_new, H[:j+1], dM_dT_H[:j+1, i]), H_new)
+        dSM_2[j, i] = S[j, i] - S[0, i]
+        dT[j, i] = t * (np.exp(-np.trapz(np.interp(H_new, H[:j+1], dM_dT_H[:j+1, i] / C[:j+1, i]), H_new)) - 1)
+
+print("MCE calculations done")
+
+plt.subplots(2, 3, sharex="all")
+plt.figure(1)
+plt.subplot(2, 3, 1)
+for j, h in enumerate(H):
+    plt.plot(T, M[j, :], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$M$")
+
+plt.subplot(2, 3, 3)
+for j, h in enumerate(H):
+    plt.plot(T, S[j, :], label=h)
 plt.xlabel(r"$T$")
 plt.ylabel(r"$S$")
-plt.legend()
 
-plt.figure(3)
-plt.plot(T, U_F[0, :], label="from F min")
-plt.plot(T, E_mean[0, :], label="from T avg")
+plt.subplot(2, 3, 2)
+for j, h in enumerate(H):
+    plt.plot(T, U[j, :], label=h)
 plt.xlabel(r"$T$")
 plt.ylabel(r"$U$")
-plt.legend()
 
-plt.figure(4)
-plt.plot(T, C_F[0, :], label="from F min")
-plt.plot(T, C_mean[0, :], label="from T avg")
+plt.subplot(2, 3, 6)
+for j, h in enumerate(H):
+    plt.plot(T, C[j, :], label=h)
 plt.xlabel(r"$T$")
 plt.ylabel(r"$C$")
-plt.legend()
 
-plt.figure(5)
-plt.plot(T, X_F[0, :], label="from F min")
-plt.plot(T, X_mean[0, :], label="from T avg")
+plt.subplot(2, 3, 4)
+for j, h in enumerate(H):
+    plt.plot(T, X[j, :], label=h)
 plt.xlabel(r"$T$")
 plt.ylabel(r"$\chi$")
-plt.legend()
+
+plt.subplot(2, 3, 5)
+for j, h in enumerate(H):
+    plt.plot(T, G[j, :], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$G$")
+
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.9,
+                    wspace=0.4,
+                    hspace=0.4)
+plt.savefig(SAVE_FOLDER + f"TD_Ising_L{L}_{lattice}_npos{Sz_vals}" + SAVE_EXT)
+
+plt.subplots(1, 2, sharex="all")
+plt.figure(2)
+
+plt.subplot(1, 3, 1)
+for j, h in enumerate(H):
+    plt.plot(T, -dSM[j, :], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$- \Delta S_M$ from MR")
+# plt.legend()
+
+plt.subplot(1, 3, 2)
+for j, h in enumerate(H):
+    plt.plot(T, -dSM_2[j, :], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$- \Delta S_M$ from $\Delta S$")
+# plt.legend()
+
+plt.subplot(1, 3, 3)
+for j, h in enumerate(H):
+    plt.plot(T, dT[j, :], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$\Delta T$")
+# plt.ylim([-0.05, 0.43])
+# plt.legend()
+
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1,
+                    right=0.9,
+                    top=0.9,
+                    wspace=0.4,
+                    hspace=0.4)
+plt.savefig(SAVE_FOLDER + f"MCE_Ising_L{L}_{lattice}_npos{Sz_vals}" + SAVE_EXT)
 
 plt.show()
