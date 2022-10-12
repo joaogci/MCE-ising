fb8130d7cd87be12f037b5010b1e32feef505d02
João Inácio <j.g.c.inacio@fys.uio.no>
2022-10-12 13:37:03 +0200

    added saving computed Z to a folder and plot color scheme

--- therm_ising.py
+++ therm_ising.py
@@ -2,9 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from tqdm import tqdm
+import os
 
-SAVE_FOLDER = "figures/"
-SAVE_EXT = ".pdf"
+SAVE_FOLDER = lambda L, lattice, Sz_vals: f"results/MCE_L{L}_{lattice}_npos{Sz_vals}/"
+SAVE_FOLDER_FIGS = lambda L, lattice, Sz_vals: f"figures/MCE_L{L}_{lattice}_npos{Sz_vals}/"
+SAVE_EXT_FIGS = ".pdf"
+SAVE_EXT = ".dat"
 
 NN_list = {"SS": 4, "SC": 6, "HCP": 12, "Hex": 8, "FCC": 12, "BCC": 8}
 N_list = {"SS": lambda L: L**2, "SC": lambda L: L**3, "HCP": lambda L: 4*L**3, "Hex": lambda L: L**3, "FCC": lambda L: 4*L**3, "BCC": lambda L: 2*L**3}
@@ -23,27 +26,22 @@ J = 1.0
 H_plot = [0.01, 0.02, 0.03, 0.04, 0.05, 0.1]
 
 T_max = 5.0
-dT = 0.05
+T_vals = 64
 H_max = 0.1
-dH = 0.002
+H_vals = 21
 
-T = np.arange(dT, T_max + dT, dT)
-H = np.arange(0.0, H_max + dH, dH)
-T_vals = len(T)
-H_vals = len(H)
+# Temperature, field, volume and J
+T = np.arange(T_max / T_vals, T_max + T_max / T_vals, T_max / T_vals)
+H = np.arange(0.0, H_max + H_max / (H_vals - 1), H_max / (H_vals - 1))
 
 print("Computing thermodynamic variables for: ")
 print(f" L: {L} | lattice: {lattice} | N: {N} | S: {S} | J: {J}")
 print(f" Ti: {T[0]} | Tf: {T[-1]} | nT: {len(T)}")
 print(f" Hi: {H[0]} | Hf: {H[-1]} | nH: {len(H)}")
 
-# Hamiltonian function
-def Hamiltonian(J, E, h, M):
-    return J * E - h * M
-
 # Energy and magnetization values and JDOS
-max_E = 4.0 * J * S**2 * NN * N / 2.0
-dE = 4 * J
+max_E = 4.0 * S**2 * NN * N / 2.0
+dE = 4
 max_M = 2 * S * N
 dM = 2
 
@@ -63,17 +61,25 @@ print("JDOS read")
 # Partition function
 ln_ZM = np.zeros((H_vals, T_vals, M_vals))
 
-print(f"iterations for Z: {H_vals * M_vals * T_vals}")
+print("Checking if ln_Z is already computed")
+if os.path.isdir(SAVE_FOLDER(L, lattice, Sz_vals)):
+    for q, m in enumerate(M_sys):
+        ln_ZM[:, :, q] = np.loadtxt(SAVE_FOLDER(L, lattice, Sz_vals) + f"lnZ_q{q}" + SAVE_EXT)
+    print("File found.")
+else:
+    print(f"File not found. Iterations for Z: {H_vals * M_vals * T_vals}")
+    for q, m in enumerate(tqdm(M_sys)):
+        hits = np.where(g[:, q] != 0.0)[0]
+        energy = J * E_sys
 
-for j, h in enumerate(tqdm(H)):
-    for i, t in enumerate(T):
-        for q, m in enumerate(M_sys):
-            hits = np.where(g[:, q] != 0.0)[0]
-            
-            ln_ZM[j, i, q] += np.log(g[hits[0], q]) - Hamiltonian(J, E_sys[hits[0]], h, m) / t
-            ln_ZM[j, i, q] += np.log(1 + np.sum(np.exp(np.log(g[hits[1:], q]) - (Hamiltonian(J, E_sys[hits[1:]], h, m) / t) - ln_ZM[j, i, q])))
+        for j, h in enumerate(H):
+            for i, t in enumerate(T):
+                
+                cte = np.log(g[hits[0], q]) - (energy[hits[0]] - h * m) / t
+                ln_ZM[j, i, q] += cte
+                ln_ZM[j, i, q] += np.log(1 + np.sum(np.exp(np.log(g[hits[1:], q]) - ((energy[hits[1:]] - h * m) / t) - cte)))
         
-print("Computed partition function")
+    print("Computed partition function")
 
 # Minimization of G
 G_tmp = np.zeros((H_vals, T_vals, M_vals))
@@ -106,23 +112,29 @@ dM_dT_H = np.gradient(M, T, axis=1)
 
 for i, t in enumerate(T):
     for j, h in enumerate(H):
-
         dSM[j, i] = np.trapz(dM_dT_H[:j+1, i], H[:j+1])
         # dT[j, i] = t * (np.exp(-np.trapz(np.interp(H_new, H[:j+1], dM_dT_H[:j+1, i] / C[:j+1, i]), H_new)) - 1)
 
 print("MCE calculations done")
 
-# Find Tc as a function of H
-# interp M 
-Tc = np.zeros(H_vals)
-
-for j, h in enumerate(H):
-    T_interp = np.linspace(T[0], T[-1], int(1e6))
-    grad_interp = np.interp(T_interp, T, - np.gradient(M[j, :], T))
-    
-    Tc[j] = T_interp[np.where(np.max(grad_interp) == grad_interp)[0][0]]
+# Find Tc for H = 0
+T_interp = np.linspace(T[0], T[-1], int(1e7))
+grad = - np.gradient(np.interp(T_interp, T, M[0, :]), T_interp)
+Tc = T_interp[np.where(np.max(grad) == grad)[0][0]]
+print(f"Tc = {Tc}")
 
 print("Tc calculations done")
+
+# Saving results
+if not os.path.isdir(SAVE_FOLDER(L, lattice, Sz_vals)):
+    if not os.path.isdir("results/"):
+        os.mkdir("results/")
+        
+    print(f"Saving Z on {SAVE_FOLDER(L, lattice, Sz_vals)}lnZ{SAVE_EXT}")
+    os.mkdir(SAVE_FOLDER(L, lattice, Sz_vals))
+    
+    for q, m in enumerate(M_sys):
+        np.savetxt(SAVE_FOLDER(L, lattice, Sz_vals) + f"lnZ_q{q}" + SAVE_EXT, ln_ZM[:, :, q])
 
 # Global plotting options
 plt.style.use('seaborn')
@@ -130,23 +142,29 @@ matplotlib.rcParams['mathtext.fontset'] = 'stix'
 matplotlib.rcParams['lines.marker'] = '.'
 matplotlib.rcParams['lines.linestyle'] = '-'
 matplotlib.rcParams['lines.linewidth'] = '1'
-matplotlib.rcParams['lines.markersize'] = '5'
+matplotlib.rcParams['lines.markersize'] = '10'
 matplotlib.rcParams['savefig.bbox'] = 'tight'
 matplotlib.rcParams['figure.subplot.left'] = '0.1'
 matplotlib.rcParams['figure.subplot.bottom'] = '0.1'
-matplotlib.rcParams['figure.subplot.right'] = '0.9'
-matplotlib.rcParams['figure.subplot.top'] = '0.9'
+matplotlib.rcParams['figure.subplot.right'] = '0.97'
+matplotlib.rcParams['figure.subplot.top'] = '0.97'
 matplotlib.rcParams['figure.subplot.wspace'] = '0.4'
-matplotlib.rcParams['figure.subplot.hspace'] = '0.4'
+matplotlib.rcParams['figure.subplot.hspace'] = '0.2'
+
+if not os.path.isdir(SAVE_FOLDER_FIGS(L, lattice, Sz_vals)):
+    if not os.path.isdir("figures/"):
+        os.mkdir("figures/")
+    os.mkdir(SAVE_FOLDER_FIGS(L, lattice, Sz_vals))
 
 # Plots
 plt.subplots(1, 3)
 plt.figure(1)
 
 plt.subplot(1, 3, 1)
-colors = plt.cm.viridis(np.linspace(0,1,T_vals//2))
+skip = 2
+colors = plt.cm.viridis(np.linspace(0, 1, T_vals // skip))
 for i, t in enumerate(T):
-    if i % 2 == 0:
+    if i % skip == 0:
         plt.plot(H, M[:, i], color=colors[i//2])
 plt.plot(H, M[:, 0], color=colors[0], label=f"T = {T[0]}")
 plt.plot(H, M[:, -1], color=colors[-1], label=f"T = {T[-1]}")
@@ -155,7 +173,7 @@ plt.ylabel(r"$M$")
 plt.legend()
 
 plt.subplot(1, 3, 2)
-colors = plt.cm.viridis(np.linspace(0,1,len(H_plot) + 1))
+colors = plt.cm.viridis(np.linspace(0, 1, len(H_plot) + 1))
 i = 0
 for j, h in enumerate(H):
     if h not in H_plot and h != 0.0:
@@ -167,7 +185,7 @@ plt.ylabel(r"$M$")
 plt.legend()
 
 plt.subplot(1, 3, 3)
-colors = plt.cm.viridis(np.linspace(0,1,len(H_plot)))
+colors = plt.cm.viridis(np.linspace(0, 1, len(H_plot)))
 i = 0
 for j, h in enumerate(H):
     if h not in H_plot:
@@ -178,49 +196,55 @@ plt.xlabel(r"$T$")
 plt.ylabel(r"$-\Delta S_M$")
 plt.legend()
 
+plt.savefig(SAVE_FOLDER_FIGS(L, lattice, Sz_vals) + f"MCE_magnetization" + SAVE_EXT_FIGS)
+
+plt.subplots(2, 3, sharex="all")
 plt.figure(2)
-plt.plot(H, Tc)
-plt.xlabel(r"$H$")
-plt.ylabel(r"$T_C$")
 
-# plt.subplots(2, 3, sharex="all")
-# plt.figure(1)
-# plt.subplot(2, 3, 1)
-# for j, h in enumerate(H):
-#     plt.plot(T, M[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$M$")
+plt.subplot(2, 3, 1)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, M[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$M$")
 
-# plt.subplot(2, 3, 3)
-# for j, h in enumerate(H):
-#     plt.plot(T, S[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$S$")
+plt.subplot(2, 3, 3)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, S[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$S$")
 
-# plt.subplot(2, 3, 2)
-# for j, h in enumerate(H):
-#     plt.plot(T, U[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$U$")
+plt.subplot(2, 3, 2)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, U[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$U$")
 
-# plt.subplot(2, 3, 6)
-# for j, h in enumerate(H):
-#     plt.plot(T, C[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$C$")
+plt.subplot(2, 3, 6)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, C[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$C$")
 
-# plt.subplot(2, 3, 4)
-# for j, h in enumerate(H):
-#     plt.plot(T, X[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$\chi$")
+plt.subplot(2, 3, 4)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, X[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$\chi$")
 
-# plt.subplot(2, 3, 5)
-# for j, h in enumerate(H):
-#     plt.plot(T, G[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$G$")
+plt.subplot(2, 3, 5)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, G[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$G$")
 
-# plt.savefig(SAVE_FOLDER + f"TD_Ising_L{L}_{lattice}_npos{Sz_vals}" + SAVE_EXT)
+plt.savefig(SAVE_FOLDER_FIGS(L, lattice, Sz_vals) + f"TD" + SAVE_EXT_FIGS)
+
+print(f"Figures saved on {SAVE_FOLDER_FIGS(L, lattice, Sz_vals)}")
 
 plt.show()
--- therm_volumic_ising.py
+++ therm_volumic_ising.py
@@ -2,9 +2,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 from tqdm import tqdm
+import os
 
-SAVE_FOLDER = "figures/"
-SAVE_EXT = ".pdf"
+SAVE_FOLDER = lambda L, lattice, Sz_vals, Jp, K: f"results/GMCE_L{L}_{lattice}_npos{Sz_vals}_Jp{Jp}_K{K}/"
+SAVE_FOLDER_FIGS = lambda L, lattice, Sz_vals, Jp, K: f"figures/GMCE_L{L}_{lattice}_npos{Sz_vals}_Jp{Jp}_K{K}/"
+SAVE_EXT_FIGS = ".pdf"
+SAVE_EXT = ".dat"
 
 NN_list = {"SS": 4, "SC": 6, "HCP": 12, "Hex": 8, "FCC": 12, "BCC": 8}
 N_list = {"SS": lambda L: L**2, "SC": lambda L: L**3, "HCP": lambda L: 4*L**3, "Hex": lambda L: L**3, "FCC": lambda L: 4*L**3, "BCC": lambda L: 2*L**3}
@@ -18,34 +21,31 @@ N = N_list[lattice](L)
 NN = NN_list[lattice]
 S = (Sz_vals - 1.0) / 2.0
 
+J0 = 1.0
 Jp = 3.5
 K = 50.0
 
-T_max = 14.0
-dT = T_max / 64 # 0.05
-H_max = 0.1
-dH = H_max / 20 # 0.002
-v_max = (0.5 * NN * Jp) / K
-dv = v_max / (51 - 2) # 0.01
+H_plot = [0.01, 0.02, 0.03, 0.04, 0.05, 0.1]
 
-T = np.arange(dT, T_max + dT, dT)
-H = np.arange(0.0, H_max + dH, dH)
-v = np.arange(-dv, v_max + dv, dv)
-T_vals = len(T)
-H_vals = len(H)
-v_vals = len(v)
+T_max = 14.0
+T_vals = 64
+H_max = 0.1
+H_vals = 21
+v_max = (0.5 * NN * Jp) / K
+v_vals = 51
+
+# Temperature, field, volume and J
+T = np.arange(T_max / T_vals, T_max + T_max / T_vals, T_max / T_vals)
+H = np.arange(0.0, H_max + H_max / (H_vals - 1), H_max / (H_vals - 1))
+v = np.arange(- v_max / (v_vals - 2), v_max + v_max / (v_vals - 2), v_max / (v_vals - 2))
+J = J0 + Jp * v
 
 print("Computing thermodynamic variables for: ")
 print(f" L: {L} | lattice: {lattice} | N: {N} | S: {S} ")
-print(f" K: {K} | Jp: {Jp} ")
+print(f" K: {K} | J0: {J0} | Jp: {Jp} ")
 print(f" Ti: {T[0]} | Tf: {T[-1]} | nT: {len(T)}")
 print(f" Hi: {H[0]} | Hf: {H[-1]} | nH: {len(H)}")    
 print(f" vi: {v[0]} | vf: {v[-1]} | nv: {len(v)}")    
-
-pre_comp = 0.5 * K * N * np.power(v, 2.0)
-# Hamiltonian function
-def Hamiltonian(k, E, h, M):
-    return J[k] * E - h * M + pre_comp[k]
 
 # Energy and magnetization values and JDOS
 max_E = 4.0 * S**2 * NN * N / 2.0
@@ -55,7 +55,6 @@ dM = 2
 
 E_sys = np.arange(- max_E, max_E + dE, dE)
 M_sys = np.arange(- max_M, max_M + dM, dM)
-J = 1 + Jp * v
 M_vals = len(M_sys)
 E_vals = len(E_sys)
 
@@ -70,19 +69,33 @@ print("JDOS read")
 # Partition function
 ln_ZMv = np.zeros((H_vals, T_vals, M_vals, v_vals))
 
-print(f"iterations for Z: {H_vals * v_vals * M_vals * T_vals}")
+print("Checking if ln_Z is already computed")
+if os.path.isdir(SAVE_FOLDER(L, lattice, Sz_vals, Jp, K)):
+    print("File found")
 
-for j, h in enumerate(tqdm(H)):
-    for i, t in enumerate(T):
+    for k in range(v_vals):
+        for q, m in enumerate(M_sys):
+            ln_ZMv[:, :, q, k] = np.loadtxt(SAVE_FOLDER(L, lattice, Sz_vals, Jp, K) + f"lnZ_q{q}_k{k}" + SAVE_EXT)
+
+    print("File read")
+else:
+    print("File not found")
+    print(f"Iterations for Z: {H_vals * v_vals * M_vals * T_vals}")
+
+    for k in tqdm(range(v_vals)):
+        energy = J[k] * E_sys + 0.5 * K * N * np.power(v[k], 2.0)
+        
         for q, m in enumerate(M_sys):
             hits = np.where(g[:, q] != 0.0)[0]
             
-            for k in range(v_vals):
-                cte = np.log(g[hits[0], q]) - Hamiltonian(k, E_sys[hits[0]], h, m) / t
-                ln_ZMv[j, i, q, k] += cte
-                ln_ZMv[j, i, q, k] += np.log(1 + np.sum(np.exp(np.log(g[hits[1:], q]) - (Hamiltonian(k, E_sys[hits[1:]], h, m) / t) - cte)))
+            for j, h in enumerate(H):
+                for i, t in enumerate(T):
+                    
+                    cte = np.log(g[hits[0], q]) - (energy[hits[0]] - h * m) / t
+                    ln_ZMv[j, i, q, k] += cte
+                    ln_ZMv[j, i, q, k] += np.log(1 + np.sum(np.exp(np.log(g[hits[1:], q]) - ((energy[hits[1:]] - h * m) / t) - cte)))
 
-print("Computed partition function")
+    print("Computed partition function")
 
 # Minimization of G
 G_tmp = np.zeros((H_vals, T_vals, M_vals, v_vals))
@@ -116,8 +129,7 @@ M /= N
 
 S = - np.gradient(G, T, axis=1)
 C = T * np.gradient(S, T, axis=1)
-if H_vals > 1:
-    X = np.gradient(M, H, axis=0)
+X = np.gradient(M, H, axis=0)
 U = np.zeros((H_vals, T_vals))
 for j, h in enumerate(H):
     U[j, :] = G[j, :] + T * S[j, :]
@@ -125,20 +137,35 @@ for j, h in enumerate(H):
 print("Computed minimization of G and variables")
 
 dSM = np.zeros((H_vals, T_vals))
-dSM_2 = np.zeros((H_vals, T_vals))
 # dT = np.zeros((H_vals, T_vals))
 dM_dT_H = np.gradient(M, T, axis=1)
 
 for i, t in enumerate(T):
     for j, h in enumerate(H):
-        H_new = np.arange(H[0], H[j], dH / 1000)
-
-        dSM[j, i] = np.trapz(np.interp(H_new, H[:j+1], dM_dT_H[:j+1, i]), H_new)
-        dSM_2[j, i] = S[j, i] - S[0, i]
+        dSM[j, i] = np.trapz(dM_dT_H[:j+1, i], H[:j+1])
         # dT[j, i] = t * (np.exp(-np.trapz(np.interp(H_new, H[:j+1], dM_dT_H[:j+1, i] / C[:j+1, i]), H_new)) - 1)
 
 print("MCE calculations done")
 
+# Find Tc for H = 0
+T_interp = np.linspace(T[0], T[-1], int(1e7))
+grad = - np.gradient(np.interp(T_interp, T, M[0, :]), T_interp)
+Tc = T_interp[np.where(np.max(grad) == grad)[0][0]]
+print(f"Tc = {Tc}")
+
+print("Tc calculations done")
+
+# Saving results
+if not os.path.isdir(SAVE_FOLDER(L, lattice, Sz_vals, Jp, K)):
+    if not os.path.isdir("results/"):
+        os.mkdir("results/")
+
+    print(f"Saving Z on {SAVE_FOLDER(L, lattice, Sz_vals, Jp, K)}lnZ{SAVE_EXT}")
+    os.mkdir(SAVE_FOLDER(L, lattice, Sz_vals, Jp, K))
+    
+    for k in range(v_vals):
+        for q, m in enumerate(M_sys):
+            np.savetxt(SAVE_FOLDER(L, lattice, Sz_vals, Jp, K) + f"lnZ_q{q}_k{k}" + SAVE_EXT, ln_ZMv[:, :, q, k])
 
 # Global plotting options
 plt.style.use('seaborn')
@@ -155,14 +182,20 @@ matplotlib.rcParams['figure.subplot.top'] = '0.9'
 matplotlib.rcParams['figure.subplot.wspace'] = '0.4'
 matplotlib.rcParams['figure.subplot.hspace'] = '0.4'
 
+if not os.path.isdir(SAVE_FOLDER_FIGS(L, lattice, Sz_vals, Jp, K)):
+    if not os.path.isdir("figures/"):
+        os.mkdir("figures/")
+    os.mkdir(SAVE_FOLDER_FIGS(L, lattice, Sz_vals, Jp, K))
+
 # Plots
 plt.subplots(1, 3)
 plt.figure(1)
 
 plt.subplot(1, 3, 1)
-colors = plt.cm.viridis(np.linspace(0,1,T_vals//1))
+skip = 1
+colors = plt.cm.viridis(np.linspace(0, 1, T_vals // skip))
 for i, t in enumerate(T):
-    if i % 1 == 0:
+    if i % skip == 0:
         plt.plot(H, M[:, i], color=colors[i])
 plt.plot(H, M[:, 0], color=colors[0], label=f"T = {T[0]}")
 plt.plot(H, M[:, -1], color=colors[-1], label=f"T = {T[-1]}")
@@ -171,11 +204,11 @@ plt.ylabel(r"$M$")
 plt.legend()
 
 plt.subplot(1, 3, 2)
-colors = plt.cm.viridis(np.linspace(0,1,H_vals))
+colors = plt.cm.viridis(np.linspace(0, 1, len(H_plot) + 1))
 i = 0
 for j, h in enumerate(H):
-    # if h not in H_plot and h != 0.0:
-    #     continue
+    if h not in H_plot and h != 0.0:
+        continue
     plt.plot(T, M[j, :], color=colors[i], label=f"H = {h}")
     i += 1
 plt.xlabel(r"$T$")
@@ -183,102 +216,94 @@ plt.ylabel(r"$M$")
 plt.legend()
 
 plt.subplot(1, 3, 3)
-colors = plt.cm.viridis(np.linspace(0,1,H_vals))
+colors = plt.cm.viridis(np.linspace(0, 1, len(H_plot)))
 i = 0
 for j, h in enumerate(H):
-    # if h not in H_plot:
-    #     continue:w
-    
+    if h not in H_plot:
+        continue
     plt.plot(T, -dSM[j, :], color=colors[i], label=f"H: {H[0]} to {h}")
     i += 1
 plt.xlabel(r"$T$")
 plt.ylabel(r"$-\Delta S_M$")
 plt.legend()
 
+plt.savefig(SAVE_FOLDER_FIGS(L, lattice, Sz_vals, Jp, K) + f"GMCE_magnetization" + SAVE_EXT_FIGS)
+
+plt.subplots(1, 2)
 plt.figure(2)
-plt.plot(T, v_min[0, :])
+
+plt.subplot(1, 2, 1)
+colors = plt.cm.viridis(np.linspace(0, 1, T_vals // skip))
+for i, t in enumerate(T):
+    if i % skip == 0:
+        plt.plot(H, v_min[:, i], color=colors[i])
+plt.plot(H, v_min[:, 0], color=colors[0], label=f"T = {T[0]}")
+plt.plot(H, v_min[:, -1], color=colors[-1], label=f"T = {T[-1]}")
+plt.xlabel(r"$H$")
+plt.ylabel(r"$v$")
+plt.legend()
+
+plt.subplot(1, 2, 2)
+colors = plt.cm.viridis(np.linspace(0, 1, len(H_plot) + 1))
+i = 0
+for j, h in enumerate(H):
+    if h not in H_plot and h != 0.0:
+        continue
+    plt.plot(T, v_min[j, :], color=colors[i], label=f"H = {h}")
+    i += 1
 plt.xlabel(r"$T$")
 plt.ylabel(r"$v$")
+plt.legend()
 
+plt.savefig(SAVE_FOLDER_FIGS(L, lattice, Sz_vals, Jp, K) + f"GMCE_volume" + SAVE_EXT_FIGS)
 
-# plt.subplots(2, 3, sharex="all")
-# plt.figure(1)
-# plt.subplot(2, 3, 1)
-# for j, h in enumerate(H):
-#     plt.plot(T, M[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$M$")
+plt.subplots(2, 3, sharex="all")
+plt.figure(3)
 
-# plt.subplot(2, 3, 3)
-# for j, h in enumerate(H):
-#     plt.plot(T, S[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$S$")
+plt.subplot(2, 3, 1)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, M[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$M$")
 
-# plt.subplot(2, 3, 2)
-# for j, h in enumerate(H):
-#     plt.plot(T, U[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$U$")
+plt.subplot(2, 3, 3)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, S[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$S$")
 
-# plt.subplot(2, 3, 6)
-# for j, h in enumerate(H):
-#     plt.plot(T, C[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$C$")
+plt.subplot(2, 3, 2)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, U[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$U$")
 
-# plt.subplot(2, 3, 4)
-# for j, h in enumerate(H):
-#     plt.plot(T, X[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$\chi$")
+plt.subplot(2, 3, 6)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, C[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$C$")
 
-# plt.subplot(2, 3, 5)
-# for j, h in enumerate(H):
-#     plt.plot(T, G[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$G$")
+plt.subplot(2, 3, 4)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, X[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$\chi$")
 
-# plt.subplots_adjust(left=0.1,
-#                     bottom=0.1,
-#                     right=0.9,
-#                     top=0.9,
-#                     wspace=0.4,
-#                     hspace=0.4)
-# # plt.savefig(SAVE_FOLDER + f"TD_Ising_L{L}_{lattice}_npos{Sz_vals}" + SAVE_EXT)
+plt.subplot(2, 3, 5)
+colors = plt.cm.viridis(np.linspace(0, 1, H_vals))
+for j, h in enumerate(H):
+    plt.plot(T, G[j, :], color=colors[j], label=h)
+plt.xlabel(r"$T$")
+plt.ylabel(r"$G$")
 
+plt.savefig(SAVE_FOLDER_FIGS(L, lattice, Sz_vals, Jp, K) + f"TD" + SAVE_EXT_FIGS)
 
-# plt.subplots(1, 2, sharex="all")
-# plt.figure(2)
-
-# plt.subplot(1, 2, 1)
-# for j, h in enumerate(H):
-#     plt.plot(T, -dSM[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$- \Delta S_M$ from MR")
-# # plt.legend()
-
-# plt.subplot(1, 2, 2)
-# for j, h in enumerate(H):
-#     plt.plot(T, -dSM_2[j, :], label=h)
-# plt.xlabel(r"$T$")
-# plt.ylabel(r"$- \Delta S_M$ from $\Delta S$")
-# # plt.legend()
-
-# # plt.subplot(1, 3, 3)
-# # for j, h in enumerate(H):
-# #     plt.plot(T, dT[j, :], label=h)
-# # plt.xlabel(r"$T$")
-# # plt.ylabel(r"$\Delta T$")
-# # # plt.ylim([-0.05, 0.43])
-# # # plt.legend()
-
-# plt.subplots_adjust(left=0.1,
-#                     bottom=0.1,
-#                     right=0.9,
-#                     top=0.9,
-#                     wspace=0.4,
-#                     hspace=0.4)
-# # plt.savefig(SAVE_FOLDER + f"MCE_Ising_L{L}_{lattice}_npos{Sz_vals}" + SAVE_EXT)
+print(f"Figures saved on {SAVE_FOLDER_FIGS(L, lattice, Sz_vals, Jp, K)}")
 
 plt.show()
